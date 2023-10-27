2f9047fc1dfc1dc048d70244611f7667e4eb3df0
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-27 04:36:54 -0700

    solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let res = false
+    if (year%4===0) {
+        res = true
+        if (year%100===0) {
+            res = false
+            if (year%400===0) {
+                res = true
+            }
+        }
+    }
+    return res
 };
 
 // Do not edit below this line
