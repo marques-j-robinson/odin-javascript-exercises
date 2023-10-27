744a688e2a365c2cc30e50a7406d21d70c090577
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-27 04:15:52 -0700

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...itemsToRemove) {
+    const args = [...itemsToRemove]
+    return arr.filter(i => {
+        let res = true
+        args.forEach(itemToRemove => {
+            if (itemToRemove === i) {
+                res = false
+            }
+        })
+        return res
+    })
 };
 
 // Do not edit below this line
