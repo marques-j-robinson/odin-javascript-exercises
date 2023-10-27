d5733352b791d80f32a4b8cb4a25b365574c1cee
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-27 04:44:53 -0700

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const round = temp => parseFloat(temp.toFixed(1))
+
+const convertToCelsius = function(temp) {
+    return round((temp-32)*(5/9))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    return round((temp*(9/5))+32)
 };
 
 // Do not edit below this line
