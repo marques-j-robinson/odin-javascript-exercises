4729e45ac73a7b49a811f08d6542845dce76c7cd
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-27 04:30:28 -0700

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const validateNums = nums => {
+    const validNums = nums.filter(i => typeof(i) === 'number')
+    return nums.length === validNums.length
+}
 
+const sumAll = function(...args) {
+    const nums = [...args]
+    if (!validateNums(nums)) {
+        return 'ERROR'
+    }
+    nums.sort()
+    let sum = 0
+    let start = nums[0]
+    const end = nums[nums.length-1]
+    while (start <= end) {
+        sum += start
+        ++start
+    }
+    if (sum < 0) {
+        return 'ERROR'
+    }
+    return sum
 };
 
 // Do not edit below this line
