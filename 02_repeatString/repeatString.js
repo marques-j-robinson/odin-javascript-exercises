7bc9ab7c58099639b33cba46b8c16d055110adbb
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-27 03:50:19 -0700

    solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let res = ''
+    let i = 0
+    while (i < num) {
+        res = res + s
+        ++i
+    }
+    return res
 };
 
 // Do not edit below this line
