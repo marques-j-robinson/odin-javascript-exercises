57f0c8fd635458e8d29b7923ccf1c6981f2662bf
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-02 17:39:59 -0700

    solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	const curYear = 2023
+	const filterPeople = people.map(p => {
+		p.age = p.yearOfDeath ? p.yearOfDeath - p.yearOfBirth : curYear - p.yearOfBirth
+		return p
+	})
+	filterPeople.sort((a, b) => b.age - a.age)
+	return filterPeople[0]
 };
 
 // Do not edit below this line
