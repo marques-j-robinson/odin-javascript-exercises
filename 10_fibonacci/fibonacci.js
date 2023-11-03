06e1e6322b58b2c87de1c350f84db108e62868f8
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-02 17:28:41 -0700

    solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+/*
+var i;
+var fib = [0, 1]; // Initialize array!
 
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
+*/
+
+const fibonacci = function(pos) {
+	if (pos < 0) {
+		return 'OOPS'
+	}
+	const fib = [0, 1]
+	for (let i = 2; i<=pos; i++) {
+		fib[i] = fib[i-2]+fib[i-1]
+	}
+	return fib[pos]
 };
 
 // Do not edit below this line
