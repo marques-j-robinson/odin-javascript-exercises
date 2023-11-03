59cfb799c8e747fff48029a9cead099b9728f3cd
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-02 17:00:49 -0700

    solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b	
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, i) => acc+i, 0)	
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	return nums.reduce((acc, i) => acc*i, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+		return -1
+	}
+	if (num === 0) {
+		return 1
+	}
+	return (num * factorial(num-1))
 };
 
 // Do not edit below this line
