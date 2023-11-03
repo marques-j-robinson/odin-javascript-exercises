73524f66373acc7f18e390d61942a9fff435907e
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-02 17:23:25 -0700

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+	const filterWords = words
+		.toLowerCase()
+		.replace(/[^\w]/gi, '')
+	let newWords = ''
+	for (let i = filterWords.length-1; i >= 0; i--) {
+		newWords += filterWords[i]
+	}
+	return newWords === filterWords
 };
 
 // Do not edit below this line
